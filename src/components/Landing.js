bfe593bc6d9c851805baa85701428189fba2a073
Simon Chae <s13chae@gmail.com>
2020-11-10 14:35:36 -0700

    landing structure

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const Landing = () => (
   <div className="landing panels">
-    <div className="landingheadercontainer">
-      <h2 className="landingheader"> Featured </h2>
+    <div className="landingworkcontainer">
+      <h2 className="landingwork"> Featured Work</h2>
     </div>
     <div className="landingbodycontainer">
       <div className="landingbody"></div>
+    </div>
+    <div className="landingblurbcontainer">
+      <span className="landingblurb"> Blurb </span>
+    </div>
+    <div className="landinglifecontainer">
+      <h2 className="landinglife"> Featured Life</h2>
     </div>
   </div>
 )
