9096384573aa39c44a8c09a6d021af1e2d4e3bfa
Simon Chae <s13chae@gmail.com>
2020-11-14 22:13:58 -0700

    feature grid display

--- src/components/FeaturedTile.js
+++ src/components/FeaturedTile.js
@@ -1,25 +1,16 @@
 import React from 'react';
 
-// const FeaturedTile = () => (
-//   <div className="featuredtilecontainer">
-//     <div className="featuredtileimage">
-//       <a>
-//         <img></img>
-//       </a>
-//     </div>
-//     <div className="featuredtiletitle">
-//       <h2>title</h2>
-//     </div>
-//   </div>
-// )
-
 const FeaturedTile = () => (
+  <div className="featuredtilecontainer">
     <div className="featuredtileimage">
       <a>
         <img></img>
       </a>
+    </div>
+    <div className="featuredtiletitle">
       <h2>title</h2>
     </div>
+  </div>
 )
 
 export default FeaturedTile;
