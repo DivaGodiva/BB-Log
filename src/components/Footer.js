52bdce74071906215838fec104a689af846aa611
Simon Chae <s13chae@gmail.com>
2020-11-27 21:41:03 -0700

    footer start

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="footer panels">
-    <h2> This foot about to go in your ass. </h2>
+  <div className="footercontainer panels">
+    <div className="footer">
+      <div className="copyrightcontainer">
+        <h2 className="copyright">Created by me © Godivanbuns</h2>
+      </div>
+      <div className="socialscontainer">
+        <a className="socials" href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa fa-linkedin-square"></i>
+        </a>
+        <a className="socials" href="https://github.com/DivaGodiva" target="_blank" rel="noopener noreferrer">
+          <i className="fa fa-github"></i>
+        </a>
+        <a className="socials" href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">
+          <i className="fa fa-instagram"></i>
+        </a>
+      </div>
+    </div>
   </div>
 )
 
