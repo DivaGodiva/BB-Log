95080f45644c73f6782b75fb709f83d190bd8964
Simon Chae <s13chae@gmail.com>
2021-02-01 23:44:16 -0800

    blog post outline

---
+++ src/components/Blogpost.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Blogpost = () => (
+  <div className="blogpostcontainer">
+    <div className="blogpost">
+      <div className="blogposttitlecontainer">
+        <h2 className="blogposttitle"> some blog post TITLE </h2>
+      </div>
+      <div className="blogpostbodycontainer">
+        <p className="blogpostbody"> some blog post CONTENT</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Blogpost;
